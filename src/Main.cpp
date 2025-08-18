8fb4cdbdf2081f83b2f7c3cdda569a30c61e988b
Mini <mini@minizbot.org>
2025-08-18 12:06:56 -0400

    VR & Build Pipeline

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,6 @@
 #include <Config.h>
 #include <Hooks.h>
-
+#if defined(BUILD_SKYRIMAE) || defined(BUILD_SKYRIMSE)
 extern "C" DLLEXPORT auto SKSEPlugin_Version = []() {
     SKSE::PluginVersionData v;
     v.PluginVersion({ 1, 0, 0, 0 });
@@ -10,7 +10,21 @@ extern "C" DLLEXPORT auto SKSEPlugin_Version = []() {
     v.UsesNoStructs();
     return v;
 }();
-
+#elif defined(SKYRIM_VR)
+extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info) {
+    a_info->infoVersion = SKSE::PluginInfo::kVersion;
+    a_info->name = "LightingUtil";
+    a_info->version = 0x10000000;
+    if(a_skse->IsEditor()) {
+        return false;
+    }
+    const auto ver = a_skse->RuntimeVersion();
+    if(ver <SKSE::RUNTIME_VR_1_4_15) {
+        return false;
+    }
+    return true;
+};
+#endif
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
 {
     SKSE::Init(a_skse);
